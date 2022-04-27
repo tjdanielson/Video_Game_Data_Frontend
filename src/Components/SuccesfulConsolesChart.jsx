cf9e6e43e5d7185269c074fa2c228203673c8ef1
tjdanielson <tessajdanielson@gmail.com>
2022-04-27 14:38:39 -0500

    Built out platform by global sales chart with data.

--- src/Components/SuccesfulConsolesChart.jsx
+++ src/Components/SuccesfulConsolesChart.jsx
@@ -4,23 +4,38 @@ import { Chart } from "react-google-charts";
 import { useEffect, useState } from "react";
 
 const SuccessfulConsolesChart = (props) => {
+  const [chartData, setChartData] = useState([]);
+
+  function generateDataForChart() {
+    const data = [["Platform", "Sales"], ...chartData];
+    return data;
+  }
+
   useEffect(() => {
     props.getData();
+    var result = Object.entries(props.data);
+    setChartData(result);
+    console.log(result);
   }, []);
 
-  console.log(props.data);
+  useEffect(() => {
+    if (props.data) {
+      var result = Object.entries(props.data);
+      setChartData(result);
+      console.log(result);
+    }
+  }, [props.data]);
 
-  const chartInfo = [
-    ["Element", "Density", { role: "style" }],
-    ["Copper", 8.94, "#b87333"], // RGB value
-    ["Silver", 10.49, "silver"], // English color name
-    ["Gold", 19.3, "gold"],
-    ["Platinum", 21.45, "color: #e5e4e2"], // CSS-style declaration
-  ];
   //<Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
   return (
     <div>
-      <p>Chart coming soon!!!!!!</p>
+      <p>Platform By Global Sales - In Millions</p>
+      <Chart
+        chartType="ColumnChart"
+        width="100%"
+        height="400px"
+        data={generateDataForChart()}
+      />
     </div>
   );
 };
